aee99b453886dc59aebf36e60540b3d729fe93c3
Lawrence Fu <lawrencefu@wifi-131-179-7-126.host.ucla.edu>
2020-02-07 21:33:19 -0800

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Lawrence Fu on 2/7/20.
+//  Copyright © 2020 Lawrence Fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
